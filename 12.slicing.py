7a4ec5cd29c1ba48d02549d3bb993006206a8480
nileshsingal <nileshsingal7@gmail.comgit config --global user.name nileshsingalgit commit -mgit config --global user.email nileshsingal7@gmail.com>
2020-05-15 20:58:55 +0530

    slicing

---
+++ 12.slicing.py
@@ -0,0 +1,18 @@
+'''
+12. Write a Python program to print a specified list after removing the 0th, 4th and 5th elements. 
+Sample List : ['Red', 'Green', 'White', 'Black', 'Pink', 'Yellow']
+Expected Output : ['Green', 'White', 'Black']
+
+'''
+# this solution has some limit
+'''
+sample = ['Red', 'Green', 'White', 'Black', 'Pink', 'Yellow']
+print(sample[1:4])
+'''
+
+
+# Perfect solution for this example 
+
+color = ['Red', 'Green', 'White', 'Black', 'Pink', 'Yellow']
+color = [x for (i,x) in enumerate(color) if i not in (0,4,5)]
+print(color)
