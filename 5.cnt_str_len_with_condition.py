0af2321b1bd9f64cbc990a0f4deaee8e7a0c6c71
nileshsingal <61235528+nileshsingal@users.noreply.github.com>
2020-05-15 16:56:55 +0530

    Count string elements with given condition

--- 5.cnt_str_len_with_condition.py
+++ 5.cnt_str_len_with_condition.py
@@ -1,7 +1,7 @@
 '''
 5. Write a Python program to count the number of strings where the string
 length is 2 or more and the first and last character are same from a
-given list of strings. 
+given list of strings . 
 '''
 
 
