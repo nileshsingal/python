c2277f69bd294cfa5cf682fa3f060f18dc7755b6
nileshsingal <nileshsingal7@gmail.comgit config --global user.name nileshsingalgit commit -mgit config --global user.email nileshsingal7@gmail.com>
2020-05-16 12:12:04 +0530

    random selection of ele

---
+++ 25.random_selection.py
@@ -0,0 +1,6 @@
+# 25. Write a Python program to select an item randomly from a list. 
+import random
+
+movies = ["bahubali", "iron man","superman", "don", "end game"]
+print(random.choice(movies))
+
