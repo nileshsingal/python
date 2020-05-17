f33dd1819657dadefef16b26335fd3d1e4ef1d1f
nileshsingal <nileshsingal7@gmail.comgit config --global user.name nileshsingalgit commit -mgit config --global user.email nileshsingal7@gmail.com>
2020-05-17 18:04:43 +0530

    func

--- Python functions/6.check_range.py
+++
@@ -1,9 +0,0 @@
-# 6. Write a Python function to check whether a number is in a given range.
-
-def check_range(n):
-    if n in range(3,9):
-        print("The Number ",n," is in given range")
-    else:
-        print("The Number ",n," is not in given range")
-
-check_range(7)
