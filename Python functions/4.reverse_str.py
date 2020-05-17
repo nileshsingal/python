fddbeb8f6f893d52b7d332e1fb920f149ceb0903
nileshsingal <nileshsingal7@gmail.comgit config --global user.name nileshsingalgit commit -mgit config --global user.email nileshsingal7@gmail.com>
2020-05-17 18:06:48 +0530

    function assignment

---
+++ Python functions/4.reverse_str.py
@@ -0,0 +1,10 @@
+'''
+4. Write a Python program to reverse a string. 
+Sample String : "1234abcd"
+Expected Output : "dcba4321"
+'''
+
+def rev_str(string):
+    print(str(string[::-1]))
+
+rev_str("1234abcd")
---
+++ Python functions/9.is_prime.py
@@ -0,0 +1,17 @@
+'''
+9. Write a Python function that takes a number as a parameter and check the number is prime or not. 
+Note : A prime number (or a prime) is a natural number greater than 1 and that has no positive divisors other than 1 and itself.
+'''
+def is_prime(n):
+    if  n < 2:
+        return False
+    elif  n == 2:
+        return True
+    elif n > 2:
+        for i in range(2,n):
+            if n % i == 0:
+                return False
+        return True
+
+print(is_prime(7))
+     
