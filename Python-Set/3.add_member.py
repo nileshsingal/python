a9825a684b2e2a4b24632f933a2ab38798a7277e
nileshsingal <nileshsingal7@gmail.comgit config --global user.name nileshsingalgit commit -mgit config --global user.email nileshsingal7@gmail.com>
2020-05-18 16:43:34 +0530

    add element in set

---
+++ Python-Set/3.add_member.py
@@ -0,0 +1,9 @@
+#3. Write a Python program to add member(s) in a set.
+
+movies = set()
+movies.add("bahubali")
+print(movies)
+
+#Add multiple items
+movies.update(["avengers","parmanu"])
+print(movies)
