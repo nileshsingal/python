47cdbac1f7afd691104a7945c34efd26efe809d9
nileshsingal <nileshsingal7@gmail.comgit config --global user.name nileshsingalgit commit -mgit config --global user.email nileshsingal7@gmail.com>
2020-05-18 16:41:53 +0530

    creating set

---
+++ Python-Set/1.create_set.py
@@ -0,0 +1,22 @@
+#1. Write a Python program to create a set.
+
+# creating empty set
+'''
+
+xset = {0}
+print(type(xset))
+
+
+'''
+
+# Method 2
+
+'''
+x = set([0,1,2,3])
+print(type(x))
+'''
+
+# Method 3
+
+x = {1,2,3,4,5}
+print(type(x))
