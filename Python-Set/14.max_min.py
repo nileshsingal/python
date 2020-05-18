12d368d5d6078d46f82e3e47d7c0101ee8692811
nileshsingal <nileshsingal7@gmail.comgit config --global user.name nileshsingalgit commit -mgit config --global user.email nileshsingal7@gmail.com>
2020-05-18 16:49:33 +0530

    finding max,min element

---
+++ Python-Set/14.max_min.py
@@ -0,0 +1,19 @@
+#14. Write a Python program to find maximum and the minimum value in a set.
+
+set1 = {17,29,30,50,64,39,275,4,3,1,46,6}
+
+max1 = max(set1)
+min1 = min(set1)
+print("Maximum element in the set is : ",max1)
+print("Minimum element in the set is : ",min1)
+
+
+# Alternate Method
+
+'''
+
+set1 = {17,29,30,50,64,39,275,4,3,1,46,6}
+print("Maximum element in the set is : ",max(set1))
+print("Minimum element in the set is : ",min(set1))
+
+'''
