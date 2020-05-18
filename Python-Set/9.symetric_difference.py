08812e42982ae4fa8349e0ee3f2fe296ed7f4495
nileshsingal <nileshsingal7@gmail.comgit config --global user.name nileshsingalgit commit -mgit config --global user.email nileshsingal7@gmail.com>
2020-05-18 16:46:52 +0530

    find symmetric difference

---
+++ Python-Set/9.symetric_difference.py
@@ -0,0 +1,17 @@
+# 9. Write a Python program to create a symmetric difference. 
+
+set1 = {1, 2, 3, 4, 5}
+set2 = {6, 7, 3, 9, 4}
+
+sym_diff = set1 ^ set2
+print(sym_diff)
+
+
+
+# alternate way
+
+'''
+set_A = {1, 2, 3, 4, 5} 
+set_B = {6, 7, 3, 9, 4} 
+print(set_A.symmetric_difference(set_B))
+'''
