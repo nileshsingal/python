49072c14d4615b788b6a105d8c0d253bb767b590
nileshsingal <61235528+nileshsingal@users.noreply.github.com>
2020-05-18 19:41:31 +0530

    check number is prime or not

--- Python functions/9.is_prime.py
+++ Python functions/9.is_prime.py
@@ -1,6 +1,6 @@
 '''
 9. Write a Python function that takes a number as a parameter and check the number is prime or not. 
-Note : A prime number (or a prime) is a natural number greater than 1 and that has no positive divisors other than 1 and itself.
+Note : A prime number (or a prime) is a natural number greater than 1 and that has no positive divisors other than 1 and itself. 
 '''
 def is_prime(n):
     if  n < 2:
