6448207418ff31f177c56e8a819804499d89f03a
nileshsingal <nileshsingal7@gmail.comgit config --global user.name nileshsingalgit commit -mgit config --global user.email nileshsingal7@gmail.com>
2020-05-18 16:46:16 +0530

    difference in sets

---
+++ Python-Set/8.difference.py
@@ -0,0 +1,8 @@
+# 8. Write a Python program to create set difference. 
+set1 = {'a', 'b', 'c', 'd', 'e' }
+set2 = {'a', 'e', 'f', 'g'}
+
+diff = set1 - set2
+diff2 = set2 - set1
+print(diff)
+print(diff2)
