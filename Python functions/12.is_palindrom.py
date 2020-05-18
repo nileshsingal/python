16a173ea0242b48e246e01e8a9531f48db059f9d
nileshsingal <61235528+nileshsingal@users.noreply.github.com>
2020-05-18 19:40:58 +0530

    is  palindrom or not

--- Python functions/12.is_palindrom.py
+++ Python functions/12.is_palindrom.py
@@ -1,6 +1,6 @@
 '''
 12. Write a Python function that checks whether a passed string is palindrome or not. 
-Note: A palindrome is a word, phrase, or sequence that reads the same backward as forward, e.g., madam or nurses run.
+Note: A palindrome is a word, phrase, or sequence that reads the same backward as forward, e.g., madam or nurses run. 
 '''
 
 def is_palindrom(string):
