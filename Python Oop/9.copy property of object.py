d6fad5e0935e174a2014ec9d765557beaddb12f8
nileshsingal <nileshsingal7@gmail.com>
2020-05-19 16:34:52 +0530

    copy property of object

---
+++ Python Oop/9.copy property of object.py
@@ -0,0 +1,18 @@
+#How to copy all properties of an object to another object?
+
+
+class MyClass(object):
+    def __init__(self):
+        super(MyClass,self)
+        self.foo = 1
+        self.bar = 2
+
+obj1 = MyClass()
+obj2 = MyClass()
+
+obj1.foo = 25
+obj2.__dict__.update(obj1.__dict__)
+
+print(obj1.foo)
+print(obj2.foo)
+
