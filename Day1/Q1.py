0b80f87b4b5c21c72ae0254dd5e1ba732e6a3bf0
nileshsingal <nileshsingal7@gmail.comgit config --global user.name nileshsingalgit commit -mgit config --global user.email nileshsingal7@gmail.com>
2020-05-22 14:40:11 +0530

    Number % 7 and  5

---
+++ Day1/Q1.py
@@ -0,0 +1,19 @@
+'''
+Q1 Write a program which will find all such numbers which are divisible by 7 but are not a multiple of 5,
+between 2000 and 3200 (both included).
+The numbers obtained should be printed in a comma-separated sequence on a single line.
+
+Hints: Consider use range(#begin, #end) method
+
+'''
+
+
+begin = 2000
+end = 3201
+
+for num in range(begin,end):
+    if num % 7 == 0 and num % 5 != 0:
+        
+            print(num,end=",")
+print("")
+
