272b4230b63b257c24e1b775480e3c934364b9d8
nileshsingal <nileshsingal7@gmail.comgit config --global user.name nileshsingalgit commit -mgit config --global user.email nileshsingal7@gmail.com>
2020-05-22 14:44:42 +0530

    Age restriction

---
+++ Day1/Q5.py
@@ -0,0 +1,16 @@
+'''
+Question 5:
+
+program that grants access only to kids aged between 8-12
+using  if  else statement
+Hint::
+If aged between 8 to 12 then you are allowed… welcome!!
+Otherwise Sorry not allowed ..Bye!
+'''
+
+age = int(input("Enter Your Age :"))
+
+if 8 <= age <= 12:
+    print("you are allowed… welcome!!")
+else:
+    print("Sorry not allowed ..Bye!")
