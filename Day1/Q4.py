60f60d524cae8821faf0e43f99939e2134638c7f
nileshsingal <nileshsingal7@gmail.comgit config --global user.name nileshsingalgit commit -mgit config --global user.email nileshsingal7@gmail.com>
2020-05-22 14:43:54 +0530

    Check whether a number is even or odd

---
+++ Day1/Q4.py
@@ -0,0 +1,28 @@
+'''
+Question 4:
+
+Write a program to check wheather number is even or odd using if 
+Else statement…
+'''
+
+num = int(input("Enter number to check even or odd: "))
+if num % 2 == 0:
+    print(num," is Even Number")
+else:
+    print(num," is Odd Number")
+
+
+
+
+# using function
+
+'''
+def check_no(n):
+    if n % 2 == 0:
+        print(n," is Even Number")
+    else:
+        print(n," is Odd Number")
+
+check_no(78)
+'''
+        
