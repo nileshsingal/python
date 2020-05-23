9b12d1cae80bedf77902560029debb26861c7974
nileshsingal <nileshsingal7@gmail.comgit config --global user.name nileshsingalgit commit -mgit config --global user.email nileshsingal7@gmail.com>
2020-05-23 12:48:07 +0530

    take n and print table

---
+++ Day2/Q3.py
@@ -0,0 +1,7 @@
+'''
+Write a Python program to create the multiplication table (from 1 to 10)
+of a number. 
+'''
+n = int(input("Enter number for table : "))
+for i in range(1,11):
+    print(n ,"x", i ,"=", n * i)
