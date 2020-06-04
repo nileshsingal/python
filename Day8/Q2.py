8e5cdd61b2f81e2ac64b1a9ae2736a8ee271f7b9
nileshsingal <nileshsingal7@gmail.comgit config --global user.name nileshsingalgit commit -mgit config --global user.email nileshsingal7@gmail.com>
2020-06-04 10:29:54 +0530

    not possible in python

---
+++ Day8/Q2.py
@@ -0,0 +1,5 @@
+# Write a program to implement Constructor Overloading.
+
+No Constructor Overloading in Python
+If you give it more than one constructor,
+that does not lead to constructor overloading in Python.
